5c47a244b3769c573f0c3cd78042161c5eef9f21
Ebadparkar <ebadparkar1911@gmail.com>
2023-07-10 06:13:41 -0700

    otp

---
+++ app/src/main/java/com/example/openfashion/otp_verification_page.java
@@ -0,0 +1,13 @@
+package com.example.openfashion;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class otp_verification_page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_otp_verification_page);
+    }
+}
