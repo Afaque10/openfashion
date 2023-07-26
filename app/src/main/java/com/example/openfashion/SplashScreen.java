60f80db92589a4f76ffeff444f4a2299a2eee6be
Ebadparkar <ebadparkar1911@gmail.com>
2023-07-26 06:20:24 -0700

    fashion

--- app/src/main/java/com/example/openfashion/SplashScreen.java
+++ app/src/main/java/com/example/openfashion/SplashScreen.java
@@ -1,13 +1,34 @@
 package com.example.openfashion;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Handler;
+import android.os.Looper;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class SplashScreen extends AppCompatActivity {
 
+    final Handler handler = new Handler(Looper.getMainLooper());//handler object creation
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getBaseContext());
+        boolean previouslyStarted = prefs.getBoolean(getString(R.string.pref_previously_started), false);
+
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                    Intent a = new Intent(getApplicationContext(), otp_verification_page.class);//intent object creation
+                    a.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);//registering flags
+                    startActivity(a);//starting the intent
+                    finish();
+                }
+        }, 1500);
     }
 }
--- app/src/main/java/com/example/openfashion/otp_verification_page.java
+++ app/src/main/java/com/example/openfashion/otp_verification_page.java
@@ -2,12 +2,36 @@ package com.example.openfashion;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 
 public class otp_verification_page extends AppCompatActivity {
+
+    EditText phonenumberedittext,otpone,otptwo,otpthree,otpfour,otpfive,otpsix;
+    Button sendotp,verifotp;
+    ProgressBar prgsendotp,prgs
+
+
+
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_otp_verification_page);
+
+        phonenumberedittext = (EditText) findViewById(R.id.phonenumberedittext);
+
+        otpone = (EditText) findViewById(R.id.otpdigit1);
+        otptwo = (EditText) findViewById(R.id.otpdigit2);
+        otpthree = (EditText) findViewById(R.id.otpdigit3);
+        otpfour = (EditText) findViewById(R.id.otpdigit4);
+        otpfive = (EditText) findViewById(R.id.otpdigit5);
+        otpsix = (EditText) findViewById(R.id.otpdigit6);
+
     }
 }
