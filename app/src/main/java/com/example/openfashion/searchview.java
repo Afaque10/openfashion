ae48b3f84cd45128abeaf6849d836cc46fb885a0
Ebadparkar <ebadparkar1911@gmail.com>
2023-08-29 05:29:12 -0700

    searchbar

---
+++ app/src/main/java/com/example/openfashion/searchview.java
@@ -0,0 +1,14 @@
+package com.example.openfashion;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class searchview extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_searchview);
+    }
+}
