e60c2a002ec7ab7835c71fefd29240b70ee51f11
Ebadparkar <ebadparkar1911@gmail.com>
2023-09-01 04:59:15 -0700

    dark

---
+++ app/src/main/java/com/example/openfashion/black_collection_detail.java
@@ -0,0 +1,14 @@
+package com.example.openfashion;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class black_collection_detail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_black_collection_detail);
+    }
+}
