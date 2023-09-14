db9712377bee50911f5757a6bf6de260742b5d95
Ebadparkar <ebadparkar1911@gmail.com>
2023-09-14 05:20:00 -0700

    cardmethod

---
+++ app/src/main/java/com/example/openfashion/checkoutbuyer.java
@@ -0,0 +1,14 @@
+package com.example.openfashion;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class checkoutbuyer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_checkoutbuyer);
+    }
+}
