936649ace159424e25cb7f2d41434109854636f6
Ebadparkar <ebadparkar1911@gmail.com>
2023-09-18 05:42:29 -0700

    allow

--- app/src/main/java/com/example/openfashion/SplashScreen.java
+++ app/src/main/java/com/example/openfashion/SplashScreen.java
@@ -1,31 +1,38 @@
 package com.example.openfashion;
 
-import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Handler;
 import android.os.Looper;
 import android.preference.PreferenceManager;
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 
-@SuppressLint("CustomSplashScreen")
 public class SplashScreen extends AppCompatActivity {
 
     final Handler handler = new Handler(Looper.getMainLooper());//handler object creation
+    ProgressBar Prog;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
 
+        Prog=(ProgressBar) findViewById(R.id.load);
+
 
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getBaseContext());
         boolean previousStarted = prefs.getBoolean(getString(R.string.pref_previously_started),false);
+        boolean loggedin= prefs.getBoolean(getString(R.string.user_logged_in),false);
 
 
-        // handler postdelayed method implemented
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
@@ -34,16 +41,30 @@ public class SplashScreen extends AppCompatActivity {
                     SharedPreferences.Editor edit = prefs.edit();
                     edit.putBoolean(getString(R.string.pref_previously_started), Boolean.TRUE);
                     edit.commit();
-                    Intent a = new Intent(getApplicationContext(), home_page.class);//intent object creation
+                    Intent a = new Intent(SplashScreen.this, home_page.class);//intent object creation
                     a.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);//registering flags
                     startActivity(a);//starting the intent
                     finish();
                 }
                 else{
-                    Intent a = new Intent(getApplicationContext(), otp_verification_page.class);//intent object creation
-                    a.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);//registering flags
-                    startActivity(a);//starting the intent
-                    finish();
+
+                    Intent intent = new Intent(SplashScreen.this,otp_verification_page.class);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);
+
+                    Prog.setVisibility(View.VISIBLE);
+                    handler.postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            Prog.setVisibility(View.GONE);
+                            startActivity(intent);
+                            finish();
+                        }
+                    },1500);
+
+
+
+
+
                 }
 
 
--- app/src/main/java/com/example/openfashion/home_page.java
+++ app/src/main/java/com/example/openfashion/home_page.java
@@ -1,13 +1,30 @@
 package com.example.openfashion;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class home_page extends AppCompatActivity {
+
+    ImageView searching;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+
+        searching = (ImageView) findViewById(R.id.searchterm);
+        searching.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent b = new Intent(getApplicationContext(),searchbar.class);
+                startActivity(b);
+            }
+        });
+
+
     }
 }
