b0abb1c9595d8b5fb070bc7e84f7de2b7d51923a
Ebadparkar <ebadparkar1911@gmail.com>
2023-09-19 04:55:09 -0700

    yes

--- app/src/main/java/com/example/openfashion/addnewcard.java
+++ app/src/main/java/com/example/openfashion/addnewcard.java
@@ -9,6 +9,6 @@ public class addnewcard extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.addnewcard);
+        setContentView(R.layout.activity_addnewcard);
     }
 }
